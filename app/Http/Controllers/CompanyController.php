7ef1e6d7a1a6506bbb30b5657674229269e3df46
MH-Bayzid <128625718+MH-Bayzid@users.noreply.github.com>
2023-03-24 12:23:20 +0600

    Update CompanyController.php

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -11,22 +11,25 @@ use RealRashid\SweetAlert\Facades\Alert;
 
 class CompanyController extends Controller
 {
+    // company menu view
     function companies(){
         $companies= Company::paginate(5);
         return view('admin.company.list_company',[
             'companies'=>$companies,
         ]);
     }
-
+    // company details insert into DB
     function company_store(Request $request){
+        // validation
         $request->validate([
             'name'=>'required',
-            'email'=>'required',
+            'email'=>'required|email',
             'website'=>'required',
             'logo'=>'required|max:512',
             'logo'=>'mimes:jpg,png,gif',
             
         ]);
+        // if company has no logo
         if($request->logo == ''){
             Company::insert([
                 'name'=>$request->name,
@@ -34,10 +37,11 @@ class CompanyController extends Controller
                 'website'=>$request->website,
                 'created_at'=> Carbon::now(),
             ]);
-            toast('Company Added!','success');
-            return back(); 
+            
+            return back()->withSuccess('Company Added!'); 
         } 
         else{
+            // if company has logo
             $random_number= random_int(267782,4567784);
             $company_logo= $request->logo;
             $extension= $company_logo->getClientOriginalExtension();
@@ -51,19 +55,20 @@ class CompanyController extends Controller
                 'logo'=>$file_name,
                 'created_at'=> Carbon::now(),
             ]);
-            toast('Company Added!','success');
-            return back(); 
+            return back()->withSuccess('Company Added!'); 
         }
         
     }
+    // company details edit view
     function edit_company($company_id){
         $company= Company::find($company_id);
         return view('admin.company.edit_company',[
             'company'=>$company,
         ]);
     }
-
+    // company details update
     function company_update(Request $request){
+        // if logo don't want to update
         if($request->logo == ''){
             Company::find($request->company_id)->update([
                 'name'=>$request->name,
@@ -73,7 +78,9 @@ class CompanyController extends Controller
             toast('Details Updated!','success');
             return back();
         }
+        // if logo want to update
         else{
+            // if company don't have previous logo
             if(Company::find($request->company_id)->logo == ''){
                 $random_number= random_int(267782,4567784);
                 $company_logo= $request->logo;
@@ -90,7 +97,9 @@ class CompanyController extends Controller
                 toast('Details Updated!','success');
                 return back();
             }
+            // if company has previous logo
             else{
+                // previous logo unlink code
                 $present_image = Company::find($request->company_id);
                 $delete_from = public_path('uploads/company/'.$present_image->logo);
                 unlink($delete_from);
@@ -112,14 +121,25 @@ class CompanyController extends Controller
              }
         }
     }
-
+    // company details delete
     function delete_company($company_id){
-        $present_image = Company::find($company_id);
-        $delete_from = public_path('uploads/company/'.$present_image->logo);
-        unlink($delete_from);
+        // company don't have logo
+        if(Company::find($company_id)->logo == ''){
+            Company::find($company_id)->delete();
+            toast('Company Details Deleted!','info');
+            return back();
+        }
+        else{
+        // company has logo
+            $present_image = Company::find($company_id);
+            $delete_from = public_path('uploads/company/'.$present_image->logo);
+            unlink($delete_from);
 
-        Company::find($company_id)->delete();
-        toast('Company Details Deleted!','info');
-        return back();
+            Company::find($company_id)->delete();
+            toast('Company Details Deleted!','info');
+            return back();
+        }
+        
     }
 }
+
